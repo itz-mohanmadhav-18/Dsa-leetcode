a6e092e10a05d8e5b1dfb0dd8558447e7a6d2132
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-01-05 10:42:09 +0530

    [848] [Time Beats: 91.84%] [Memory Beats: 93.25%] - LeetPush

--- 848-Shifting-Letters.java
+++ 848-Shifting-Letters.java
@@ -1,17 +1,24 @@
 // import java.utils.*;
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        int sum = 0;
-        for (int i = shifts.length - 1; i >= 0; i--) {
-            sum = (sum + shifts[i]) % 26;
-            shifts[i] = sum;
+        // int sum = 0;
+        // for (int i = shifts.length - 1; i >= 0; i--) {
+        //     sum = (sum + shifts[i]) % 26;
+        //     shifts[i] = sum;
+        // }
+        // StringBuilder replaced = new StringBuilder();
+        // for (int i = 0; i < s.length(); i++) {
+        //     char charToBeReplaced = s.charAt(i);
+        //     char newChar = (char) ('a' + (charToBeReplaced - 'a' + shifts[i]) % 26);
+        //     replaced.append(newChar);
+        // }
+        // return replaced.toString();
+        char[] chars = s.toCharArray();
+        long shift = 0;
+        for(int i = chars.length - 1 ; i>=0 ; i-- ){
+            shift = (shift+shifts[i])%26;
+            chars[i] = (char)((chars[i]-'a'+shift)%26 + 'a' );
         }
-        StringBuilder replaced = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char charToBeReplaced = s.charAt(i);
-            char newChar = (char) ('a' + (charToBeReplaced - 'a' + shifts[i]) % 26);
-            replaced.append(newChar);
-        }
-        return replaced.toString();
+        return new String(chars);
     }
 }
