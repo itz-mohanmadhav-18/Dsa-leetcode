09b555d50ad0b58d41ef8ff45f26bbdc7f78fa4f
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-01-07 19:41:17 +0530

    [1408] [Time Beats: 92.21%] [Memory Beats: 25.06%] - LeetPush

--- DCP-01-25/1408-String-Matching-in-an-Array.java
+++ DCP-01-25/1408-String-Matching-in-an-Array.java
@@ -2,11 +2,9 @@ class Solution {
     public List<String> stringMatching(String[] words) {
         // Arrays.sort(words);
         Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
-        Set<String> ans = new HashSet<>();
+        List<String> ans = new ArrayList<>();
         for(int i = 0 ; i < words.length;i++){
-            // System.out.print(words[i]+" ")
             for(int j = i+1 ; j<words.length ; j++){
-            // System.out.print(words[i]+" ");
                 if(words[j].contains(words[i])){
                        ans.add(words[i]);
                        break;
@@ -15,6 +13,6 @@ class Solution {
             }
 
         }
-        return new ArrayList<>(ans);
+        return ans;
     }
 }
