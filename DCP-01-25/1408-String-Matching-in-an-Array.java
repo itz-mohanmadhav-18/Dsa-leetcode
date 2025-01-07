8d0d97e856aebe4d6e742d4221dac9fe645f536a
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-01-07 19:47:12 +0530

    [1408] [Time Beats: 98.32%] [Memory Beats: 91.97%] - LeetPush

--- DCP-01-25/1408-String-Matching-in-an-Array.java
+++ DCP-01-25/1408-String-Matching-in-an-Array.java
@@ -3,16 +3,16 @@ class Solution {
         List<String> result = new ArrayList<>();
 
         for (int i = 0; i < words.length; i++) {
-            String curr = words[i];
-            if (isSubstringOfSomeoneButIt(words, curr, i)) {
-                result.add(curr);
+            String current = words[i];
+            if (isSubstring(words, current, i)) {
+                result.add(current);
             } 
         }
 
         return result;
     }
 
-    private static boolean isSubstringOfSomeoneButIt(String[] words, String curr, int indexToExclude) {
+    private static boolean isSubstring(String[] words, String curr, int indexToExclude) {
         for (int j = 0; j < words.length; j++) {
             if (j == indexToExclude)
                 continue;
