1ad35b7edbf557305a44df95d83a93010389678b
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-05-17 06:55:27 +0530

    [283] [Time Beats: 99.58%] [Memory Beats: 95.91%] - LeetPush

--- Move-Zeroes.java
+++ Move-Zeroes.java
@@ -1,14 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        for (int i = nums.length-1; i >= 0; i--) {
-            if (nums[i] == 0) {
-                for (int j = i; j < nums.length-1; j++) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
+        // for (int i = nums.length-1; i >= 0; i--) {
+        //     if (nums[i] == 0) {
+        //         for (int j = i; j < nums.length-1; j++) {
+        //             int temp = nums[j];
+        //             nums[j] = nums[j + 1];
+        //             nums[j + 1] = temp;
+        //         }
+        //     }
 
+        // }
+
+        // int[] arr = new int[nums.length];
+        int index = 0;
+
+        for(int x : nums){
+            if(x!=0){
+                nums[index++] = x;
+            }
+        }
+
+        for(int i = index ; i<nums.length ;i++){
+            nums[i] = 0;
         }
 
     }
