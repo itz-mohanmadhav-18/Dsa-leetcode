94cbb9116f135303e904ddef67dc6497129befdc
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-06-10 12:25:38 +0530

    Time: 1 ms (100%), Space: 42.2 MB (89.02%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
@@ -1,29 +1,45 @@
 class Solution {
     public int maxDifference(String s) {
-        if (s == null || s.length() == 0) return 0;
-
-        Map<Character, Integer> freqMap = new HashMap<>();
-        int minEven = Integer.MAX_VALUE;
-        int maxOdd = Integer.MIN_VALUE;
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
-        }
-
-        for (int freq : freqMap.values()) {
-            if (freq % 2 == 0) {
-                minEven = Math.min(minEven, freq);
-            } else {
-                maxOdd = Math.max(maxOdd, freq);
-            }
-        }
-
-        // Handle edge case: if no even or no odd frequency
-        if (minEven == Integer.MAX_VALUE || maxOdd == Integer.MIN_VALUE) {
+        if (s == null || s.length() == 0)
             return 0;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        // HashMap Implementation
+        // Map<Character,Integer> map = new HashMap<>();
+
+        // for(int i = 0 ; i<s.length() ; i++){
+        //     char x = s.charAt(i);
+        //     map.put(x,map.getOrDefault(x,0)+1);
+        // }
+        // for(int freq : map.values()){
+        //     if(freq % 2 == 0){
+        //         min = Math.min(freq,min);
+        //     }else{
+        //         max = Math.max(freq,max);
+        //     }
+        // }
+
+        // array implementation
+
+        int freq[] = new int[26];
+        for (char x : s.toCharArray()) {
+            freq[x - 'a']++;
         }
 
-        return maxOdd - minEven;
+        for (int x : freq) {
+            if (x != 0) {
+                if (x % 2 == 0) {
+                    min = Math.min(x, min);
+                } else {
+                    max = Math.max(x, max);
+                }
+            }
+
+        }
+
+        return max - min;
+
     }
 }
