f25f93e4d91b48656a47bb4a47e609dda0aa009e
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-07-08 16:49:55 +0530

    Time: 0 ms (100%), Space: 57 MB (70.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int maxSubArray(int[] arr) {
-       int maxSum = arr[0];
-        int sum = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            sum = Math.max(arr[i], sum + arr[i]);//at every point we are checking if adding this element can maximise the sum of this portion if not then drop it and start from other index
-            maxSum = Math.max(maxSum, sum);
+    public int maxSubArray(int[] arr){
+       int max = arr[0],sum = 0;
+        for(int x : arr){
+            sum += x;
+            max = sum>max?sum:max;
+            if(sum<0) sum = 0;
         }
-        return maxSum;
-        // return maxSum;
+
+        return max;
     }
 }
