ee2a14155f51efa5033e1c9669fe46a8647c7e47
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-07-09 11:56:19 +0530

    Time: 3 ms (100%), Space: 81.8 MB (20.73%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,22 +1,37 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        Queue<Integer> positive = new ArrayDeque<>();
-        Queue<Integer> negative = new ArrayDeque<>();
+        // Queue<Integer> positive = new ArrayDeque<>();
+        // Queue<Integer> negative = new ArrayDeque<>();
 
-        int[] ans = new int[nums.length];
+        // int[] ans = new int[nums.length];
 
-        for(int x : nums){
-            if(x>0){
-                positive.offer(x);
-            }else if(x<0){
-                negative.offer(x);
-            }
-        }
+        // for(int x : nums){
+        //     if(x>0){
+        //         positive.offer(x);
+        //     }else if(x<0){
+        //         negative.offer(x);
+        //     }
+        // }
+        // for(int i = 0 ; i<nums.length ; i++){
+        //     if(i%2==0){
+        //         nums[i] = positive.poll();
+        //     }else{
+        //         nums[i] = negative.poll();
+        //     }
+        // }
+
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int posIdx = 0,negIdx = 1;
         for(int i = 0 ; i<nums.length ; i++){
-            if(i%2==0){
-                ans[i] = positive.poll();
-            }else{
-                ans[i] = negative.poll();
+            if(nums[i]>0){
+                ans[posIdx] = nums[i];
+                posIdx +=2;
+            }
+            else{
+                ans[negIdx] = nums[i];
+                negIdx +=2;
             }
         }
 
