a7ba1c07d67d9cd4de5ad84250054f86cf322734
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-07-16 10:56:09 +0530

    Time: 0 ms (100%), Space: 43.1 MB (74.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,42 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int len = nums.length;
-        int nextMaxIdx = -1;
-        boolean flag = false;
-
+        boolean isNotSorted = true;
+        int nextBigIdx = -1;
         for (int i = len - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                // Step 1: Find just larger element on the right
                 for (int j = len - 1; j > i; j--) {
                     if (nums[j] > nums[i]) {
-                        nextMaxIdx = j;
+                        nextBigIdx = j;
                         break;
                     }
                 }
-                // Step 2: Swap and reverse
-                swap(nums, i, nextMaxIdx);
+                swap(nums, i, nextBigIdx);
                 reverse(nums, i + 1, len - 1);
-                flag = true;
+                isNotSorted = false;
                 break;
             }
         }
-
-        if (!flag) {
+        if (isNotSorted) {
             reverse(nums, 0, len - 1);
         }
-    }
 
-    public void reverse(int[] arr, int i, int j) {
-        for (; i < j; i++, j--) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
     }
 
     public void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    public void reverse(int[] arr, int i, int j) {
+        while (i < j) {
+            swap(arr, i++, j--);
+        }
     }
 }
