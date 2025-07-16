0dc97bdbfa3a108767ec5b478a2444677a03c3e2
Mohan Madhav SInghal <mohanmadhav448@outlook.com>
2025-07-16 12:12:45 +0530

    Time: 18 ms (88.26%), Space: 56.7 MB (87.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,43 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums.length==0) return 0;
-        TreeSet<Integer> set = new TreeSet<>();
-        int count = 1;
-        for(int i : nums){
-            set.add(i);
-        }
-        int back = set.first();
-        set.remove(back);
-        int maxlen = 1;
-        for(int x : set){
-            if(x == back+1){
-                count++;
-            }else{
-                count = 1;
-            }
-            back = x;
-            maxlen = Math.max(maxlen,count); 
-        }
+        // if (nums.length==0) return 0;
+        // TreeSet<Integer> set = new TreeSet<>();
+        // int count = 1;
+        // for(int i : nums){
+        //     set.add(i);
+        // }
+        // int back = set.first();
+        // set.remove(back);
+        // int maxlen = 1;
+        // for(int x : set){
+        //     if(x == back+1){
+        //         count++;
+        //     }else{
+        //         count = 1;
+        //     }
+        //     back = x;
+        //     maxlen = Math.max(maxlen,count); 
+        // }
 
-        return maxlen;
+        // return maxlen;
+
+        //===================================================================
+        if(nums.length==0) return 0;
+        Arrays.sort(nums);
+        int len = 1;
+        int maxLen = 1;
+        for(int i =  1 ; i<nums.length ; i++){
+            if(nums[i-1]+1==nums[i]){
+                len++;
+            }else if(nums[i]==nums[i-1]){
+                // i++;
+            }
+            else{
+                len = 1;
+            }
+            maxLen = Math.max(len,maxLen);
+        }
+       return maxLen;
     }
 }
 
